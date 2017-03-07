3223cde13ecfccc2ab44b8f4ed401d3ec11411cf
Flavius Rosu <rflavius@gmail.com>
2017-03-08 00:03:17 +0200

    create interface model for the websites

---
+++ Console/Model/Website.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Crawler Web Application
+ *
+ * @copyright  Copyright (c) 2017 Flavius Rosu (http://www.webdesignrr.ro)
+ * @version    1.0
+ */
+
+/**
+ * Website.php
+ * here we have the website interface
+ * @category   Crawler
+ * @package    Console
+ * @author     Flavius Rosu
+ */
+ 
+interface Console_Model_Website
+{
+	
+}
